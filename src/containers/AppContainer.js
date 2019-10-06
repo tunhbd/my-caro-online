b250b5e23aaddbda20e94fae44fd4dd767a04241
Nguyen Huu Tu <tunhbd1998@gmail.com>
2019-10-06 12:18:34 +0700

    Add redux reducers

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import App from '../components/App/App';
+import buildStore from '../store';
 
 export default function AppContainer() {
+  const store = buildStore();
+
   return (
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import * as actionTypes from '../actions/action-types';
+
 const initStates = {
   XPlayer: null,
   OPlayer: null,
@@ -12,8 +14,48 @@ const initStates = {
   }
 };
 
-const reducers = (state, action) => {
-  switch (action.type) {
+const reducers = (state, { type, payload }) => {
+  switch (type) {
+    case actionTypes.SET_X_PLAYER:
+      return {
+        ...state,
+        XPlayer: payload.playerName
+      };
+    case actionTypes.SET_O_PLAYER:
+      return {
+        ...state,
+        OPlayer: payload.playerName
+      };
+    case actionTypes.SET_CURRENT_PLAYER:
+      return {
+        ...state,
+        currentPlayer: payload.currentPlayer
+      };
+    case actionTypes.SET_BOARD_STATES:
+      return {
+        ...state,
+        boardStates: payload.boardStates
+      };
+    case actionTypes.SET_CURRENT_BOARD_STATE:
+      return {
+        ...state,
+        currentBoardState: payload.currentBoardState
+      };
+    case actionTypes.SET_WINNER:
+      return {
+        ...state,
+        winner: payload.winner
+      };
+    case actionTypes.SET_STATUS_MATCH:
+      return {
+        ...state,
+        isPlaying: payload.status
+      };
+    case actionTypes.SET_RESULT:
+      return {
+        ...state,
+        result: payload.result
+      };
     default:
       return state || initStates;
   }
